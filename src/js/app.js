6e5449bafe85363d81492c8d445c8193506bdd12
harvis gomez <harvisgomez17@gmail.com>
2024-11-30 21:31:12 -0400

    agregue el array de diferentes productos en el app.js

---
+++ src/js/app.js
@@ -0,0 +1,167 @@
+//PRODUCTS
+const products = [
+    //Chaquetas
+    {
+        id:"chaqueta-01",
+        tittle: "Chaqueta 01",
+        img:"",
+        category: {
+            nameItem: "CHAQUETAS",
+            id: "chaquetas"
+        },
+        price: 1000
+    },
+    {
+        id:"chaqueta-02",
+        tittle: "Chaqueta 02",
+        img:"",
+        category: {
+            nameItem: "CHAQUETAS",
+            id: "chaquetas"
+        },
+        price: 1000
+    },
+    {
+        id:"chaqueta-03",
+        tittle: "Chaqueta 03",
+        img:"",
+        category: {
+            nameItem: "CHAQUETAS",
+            id: "chaquetas"
+        },
+        price: 1000
+    },
+    {
+        id:"chaqueta-04",
+        tittle: "Chaqueta 04",
+        img:"",
+        category: {
+            nameItem: "CHAQUETAS",
+            id: "chaquetas"
+        },
+        price: 1000
+    },
+    //Jeans
+    {
+        id:"jean-01",
+        tittle: "Jean 01",
+        img:"",
+        category: {
+            nameItem: "Jeans",
+            id: "jeans"
+        },
+        price: 1000
+    },
+    {
+        id:"jean-02",
+        tittle: "Jean 02",
+        img:"",
+        category: {
+            nameItem: "Jeans",
+            id: "jeans"
+        },
+        price: 1000
+    },
+    {
+        id:"jean-03",
+        tittle: "Jean 03",
+        img:"",
+        category: {
+            nameItem: "Jeans",
+            id: "jeans"
+        },
+        price: 1000
+    },
+    {
+        id:"jean-04",
+        tittle: "Jean 04",
+        img:"",
+        category: {
+            nameItem: "Jeans",
+            id: "jeans"
+        },
+        price: 1000
+    },
+    //Shorts
+    {
+        id:"short-01",
+        tittle: "Short 01",
+        img:"",
+        category: {
+            nameItem: "Short",
+            id: "short"
+        },
+        price: 1000
+    },
+    {
+        id:"short-02",
+        tittle: "Short 02",
+        img:"",
+        category: {
+            nameItem: "Short",
+            id: "short"
+        },
+        price: 1000
+    },
+    {
+        id:"short-03",
+        tittle: "Short 03",
+        img:"",
+        category: {
+            nameItem: "Short",
+            id: "short"
+        },
+        price: 1000
+    },
+    {
+        id:"short-04",
+        tittle: "Short 04",
+        img:"",
+        category: {
+            nameItem: "Short",
+            id: "short"
+        },
+        price: 1000
+    },
+    //Shirts
+    {
+        id:"shirt-01",
+        tittle: "Shirt 01",
+        img:"",
+        category: {
+            nameItem: "Shirt",
+            id: "shirt"
+        },
+        price: 1000
+    },
+    {
+        id:"shirt-02",
+        tittle: "Shirt 02",
+        img:"",
+        category: {
+            nameItem: "Shirt",
+            id: "shirt"
+        },
+        price: 1000
+    },
+    {
+        id:"shirt-03",
+        tittle: "Shirt 03",
+        img:"",
+        category: {
+            nameItem: "Shirt",
+            id: "shirt"
+        },
+        price: 1000
+    },
+    {
+        id:"shirt-04",
+        tittle: "Shirt 04",
+        img:"",
+        category: {
+            nameItem: "Shirt",
+            id: "shirt"
+        },
+        price: 1000
+    },
+]
